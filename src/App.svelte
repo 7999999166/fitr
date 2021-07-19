721850d505a89ac0571750c5bded6c4aa85075b3
Kanik Goyal <acrotechllp@gmail.com>
2021-07-19 18:32:55 +0530

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,23 @@
 <script>
-  import {onMount} from 'svelte';
-  let count = 0;
-  onMount(() => {
-    const interval = setInterval(() => count++, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  });
+    import {Route} from 'tinro'; 
 </script>
 
-<style>
-  :global(body) {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-  }
-  .App {
-    text-align: center;
-  }
-  .App code {
-    background: #0002;
-    padding: 4px 8px;
-    border-radius: 4px;
-  }
-  .App p {
-    margin: 0.4rem;
-  }
+<nav>
+    <a href="/">Home</a>
+    <a href="/portfolio">Portfolio</a>
+    <a href="/contacts">Contacts</a>
+</nav>
 
-  .App-header {
-    background-color: #f9f6f6;
-    color: #333;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-  }
-  .App-link {
-    color: #ff3e00;
-  }
-  .App-logo {
-    height: 36vmin;
-    pointer-events: none;
-    margin-bottom: 3rem;
-    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
-  }
-  @keyframes App-logo-pulse {
-    from {
-      transform: scale(1);
-    }
-    to {
-      transform: scale(1.06);
-    }
-  }
-</style>
-
-<div class="App">
-  <header class="App-header">
-    <img src="/logo.svg" class="App-logo" alt="logo" />
-    <p>Edit <code>src/App.svelte</code> and save to reload.</p>
-    <p>Page has been open for <code>{count}</code> seconds.</p>
-    <p>
-      <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-        Learn Svelte
-      </a>
-    </p>
-  </header>
-</div>
+<Route path="/"><h1>This is the main page</h1></Route>
+<Route path="/portfolio/*">
+    <Route path="/">
+        <h1>Portfolio introduction</h1>
+        <nav>
+            <a href="/portfolio/sites">Sites</a> 
+            <a href="/portfolio/photos">Photos</a>
+        </nav>
+    </Route>
+    <Route path="/sites"><h1>Portfolio: Sites</h1></Route>
+    <Route path="/photos"><h1>Portfolio: Photos</h1></Route>
+</Route>
+<Route path="/contacts"><h1>Hello friends</h1></Route>
